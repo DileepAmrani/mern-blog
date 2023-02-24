651d74dca8568234b34b7d5ebbe6d546d8d67f6e
DileepAmrani <amranidileep44@gmail.com>
2023-02-25 02:52:23 +0500

    home page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <main>
+      <header>
+        <a href="/" className="logo">
+          Dileep's Blog
         </a>
+        <nav>
+          <a href="">Login</a>
+          <a href="">Register</a>
+        </nav>
       </header>
-    </div>
+
+      <div className="post">
+        <div className="image">
+          <img
+            src="https://techcrunch.com/wp-content/uploads/2023/02/GettyImages-1204849397.jpg?w=1390&crop=1"
+            alt=""
+          />
+        </div>
+        <div className="texts">
+          <h2>
+            TechCrunch+ roundup: Finding the right LPs, ocean conservation,
+            inside Uber’s pre-seed deck
+          </h2>
+          <p className="info">
+            <a href="" className="author">
+              Dileep Kumar
+            </a>
+            <time>2023-01-06 16:45</time>
+          </p>
+          <p className="summary">
+            The PlayStation VR2 is a simultaneously exciting and disappointing
+            development in the virtual rea...
+          </p>
+        </div>
+      </div>
+
+      <div className="post">
+        <div className="image">
+          <img
+            src="https://techcrunch.com/wp-content/uploads/2023/02/GettyImages-1204849397.jpg?w=1390&crop=1"
+            alt=""
+          />
+        </div>
+        <div className="texts">
+          <h2>
+            TechCrunch+ roundup: Finding the right LPs, ocean conservation,
+            inside Uber’s pre-seed deck
+          </h2>
+          <p className="info">
+            <a href="" className="author">
+              Dileep Kumar
+            </a>
+            <time>2023-01-06 16:45</time>
+          </p>
+          <p className="summary">
+            The PlayStation VR2 is a simultaneously exciting and disappointing
+            development in the virtual rea...
+          </p>
+        </div>
+      </div>
+
+      <div className="post">
+        <div className="image">
+          <img
+            src="https://techcrunch.com/wp-content/uploads/2023/02/GettyImages-1204849397.jpg?w=1390&crop=1"
+            alt=""
+          />
+        </div>
+        <div className="texts">
+          <h2>
+            TechCrunch+ roundup: Finding the right LPs, ocean conservation,
+            inside Uber’s pre-seed deck
+          </h2>
+          <p className="info">
+            <a href="" className="author">
+              Dileep Kumar
+            </a>
+            <time>2023-01-06 16:45</time>
+          </p>
+          <p className="summary">
+            The PlayStation VR2 is a simultaneously exciting and disappointing
+            development in the virtual rea...
+          </p>
+        </div>
+      </div>
+    </main>
   );
 }
 
